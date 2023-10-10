42b22a6a2561e18704996aead5797311307f29d9
Vitor <vitorsouzalibrelonrestini@gmail.com>
2023-10-10 08:57:28 -0300

    a

---
+++ pw2-has-assist/src/main/java/br/com/etechoracio/hasassist/repository/EquipamentoRepository.java
@@ -0,0 +1,10 @@
+package br.com.etechoracio.hasassist.repository;
+
+import br.com.etechoracio.hasassist.entity.Equipamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipamentoRepository extends JpaRepository <Equipamento, Long>{
+
+
+
+}
