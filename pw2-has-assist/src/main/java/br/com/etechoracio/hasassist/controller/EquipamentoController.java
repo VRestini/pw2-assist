23a50ad9f9a16bda2b0513a354872f74abdad0e1
Vitor <vitorsouzalibrelonrestini@gmail.com>
2023-10-17 08:59:11 -0300

    falta terminar o método de buscar através do id

--- pw2-has-assist/src/main/java/br/com/etechoracio/hasassist/controller/EquipamentoController.java
+++ pw2-has-assist/src/main/java/br/com/etechoracio/hasassist/controller/EquipamentoController.java
@@ -1,12 +1,31 @@
 package br.com.etechoracio.hasassist.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.etechoracio.hasassist.entity.Equipamento;
+import br.com.etechoracio.hasassist.repository.EquipamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/equipamentos")
 public class EquipamentoController {
 
+    @Autowired
+    private EquipamentoRepository repository;
 
+    @GetMapping
+    public List<Equipamento> listarTodos(){
+        return repository.findAll();
+    };
+
+    @GetMapping
+    public List<Equipamento> listarId(){
+        return repository.findById();
+    };
+    @PostMapping
+    public void inserir(@RequestBody Equipamento body){
+        repository.save(body);
+    };
 
 }
